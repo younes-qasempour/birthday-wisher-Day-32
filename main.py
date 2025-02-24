722e23d2ba3c6178b9f4a8a80cfc14ec640d176a
younes_qasempour <qasempouryounes@gmail.com>
2025-02-24 19:30:54 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import smtplib
+from email_pass import my_email, password
+import datetime as dt
+import random
+
+
+RECEIVER_ADDRESS = "neda22dehghan@gmail.com"
+DAY_OF_WEEK = dt.datetime.now().weekday()
+
+if DAY_OF_WEEK == 0:
+
+    with open("quotes.txt", "r") as file:
+        file_contents = file.readlines()
+        quote = random.choice(file_contents)
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=RECEIVER_ADDRESS,
+            msg=f"Subject:Hello Darling\n\n{quote}"
+        )
